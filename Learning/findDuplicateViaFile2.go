c5a1d2bd6faa8b83a18b2d879eb5fe165bda973e
Cappuccinuo <1006901532@qq.com>
2022-04-05 18:37:00 +0800

    Update the program to have ability to print the files that has duplicate

--- Learning/findDuplicateViaFile2.go
+++ Learning/findDuplicateViaFile2.go
@@ -8,8 +8,9 @@ import (
 )
 
 func main() {
-	counts := make(map[string]int)
+	fileMap := make(map[string]map[string]int)
 	for _, fileName := range os.Args[1:] {
+		counts := make(map[string]int)
 		data, err := ioutil.ReadFile(fileName)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error when read file: %v\n", err)
@@ -18,11 +19,22 @@ func main() {
 		for _, value := range strings.Split(string(data), "\n") {
 			counts[value]++
 		}
+		fileMap[fileName] = counts
 	}
-
-	for index, value := range counts {
-		if value > 1 {
-			fmt.Printf("Occurance: %d, String: %s\n", value, index)
+	var res []string
+	for fileName, dupCount := range fileMap {
+		fmt.Printf("Going through %s\n", fileName)
+		var hasDup bool
+		for index, value := range dupCount {
+			if value > 1 {
+				hasDup = true
+				fmt.Printf("Occurance: %d, String: %s\n", value, index)
+			}
 		}
+		if hasDup {
+			res = append(res, fileName)
+		}
+		fmt.Println("--------------------------")
 	}
+	fmt.Printf("The file that has duplicate are: %v\n", res)
 }
