29a80ba4cd0432ad16aa9c4f4ace230c5bf2f74e
cappuccinuo <gaoyuanbj@yahoo.com>
2023-06-27 13:48:04 +0800

    Go routine examples

---
+++ Learning/goRouting2.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var wg = sync.WaitGroup{}
+var counter = 0
+var m = sync.RWMutex{}
+
+func main() {
+	runtime.GOMAXPROCS(100)
+	for i := 0; i < 10; i++ {
+		wg.Add(2)
+		m.RLock()
+		sayHello()
+		m.RUnlock()
+		m.Lock()
+		increment()
+		m.Unlock()
+	}
+	wg.Wait()
+}
+
+func sayHello() {
+	fmt.Printf("Hello #%v\n", counter)
+	wg.Done()
+}
+
+func increment() {
+	counter++
+	wg.Done()
+}
